8f9da3ffa854f26e3022688d7b45f45cc92af136
mmxzps <mojtaba.mobasheri@hotmail.com>
2024-02-21 23:55:37 +0100

    added one eventlistener for key combination

---
+++ script.js
@@ -0,0 +1,22 @@
+const jsConfetti = new JSConfetti()
+let pressKey = '';
+window.addEventListener('keypress', (e)=> {
+  pressKey += e.key;
+  pressKey = pressKey.slice(-5);
+  if(pressKey ==='magic'){
+
+    jsConfetti.addConfetti({
+      confettiRadius: 2,
+      confettiNumber: 1500
+    })
+    .then(()=> jsConfetti.addConfetti({
+      confettiColors: [
+        '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
+      ],
+      confettiRadius: 2,
+      confettiNumber: 1500
+    }));
+
+  }
+
+})
