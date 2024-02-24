c39054ce0c9a300e17756980cd1e842a37da28cb
mmxzps <mojtaba.mobasheri@hotmail.com>
2024-02-25 00:52:34 +0100

    fixed the last eventlistener and some small touches.

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-
-
 const moj = document.getElementById("collab");
 if (moj !== null) {
   moj.addEventListener('click', () => {
-    alert('Lets collaborate!');
+    const modal = document.getElementById("myModal");
+    const span = document.getElementsByClassName("close")[0];
+
+    // Visa modalfönstret när "collab" klickas på
+    modal.style.display = "block";
+
+    // Stäng modalfönstret när användaren klickar på stäng-knappen
+    span.addEventListener('click', function() {
+      modal.style.display = "none";
+    });
+
+    // Stäng modalfönstret när användaren klickar utanför det
+    window.addEventListener('click', function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
   });
 }
+
 
 
 const jsConfetti = new JSConfetti()
